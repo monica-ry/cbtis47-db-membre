7a05498be307068a3baae433b2fde9dac3f21f28
Acobi-21 <galvez.castillo.samantha.m3s1@gmail.com>
2026-03-07 18:45:33 -0600

    Add SQL queries for user analysis and revenue report
    Added SQL queries to analyze user plans, total revenue, and payments per user.

--- queries/report_sales.sql
+++ queries/report_sales.sql
@@ -1,1 +1,20 @@
-
+Consultas de análisis de datos, mostrar usuarios con su plan
+SELECT
+ u.name AS user_name,
+ a.plan_type AS affliation_plan,
+ s.state
+FROM USER u
+JOIN SUBSCRIPTION s ON u.id_user = s.id_user
+JOIN AFFILIATION a ON s.id_affliation = a.id_affliation;
+total de ingresos
+SELECT
+ SUM(amount) AS total_revenue
+FROM PAY;
+pagos por usuario
+SELECT
+ u.name,
+ SUM(p.amount) AS total_paid
+FROM USER u
+JOIN SUBSCRIPTION s ON u.id_user = s.id_user
+JOIN PAY p ON s.id_subscription = p.id_subscription
+GROUP BY u.name;
