1ea6d66cf455aab080f5803a3fe33bb23ffdf0e4
suarezdannakaren <suarez.gonzalez.danna.karen.m351@gmail.com>
2026-03-08 15:19:29 -0600

    Add SQL commands to create and grant users

--- src/03_users.sql
+++ src/03_users.sql
@@ -1,1 +1,13 @@
+-- Administrator user
+CREATE USER 'laroca'@'localhost' IDENTIFIED BY 'quecocina';
+GRANT ALL PRIVILEGES ON *.* TO 'laroca'@'localhost';
 
+-- Application user
+CREATE USER 'elwaza'@'localhost' IDENTIFIED BY 'ostias';
+GRANT SELECT, INSERT, UPDATE ON membership_park.* TO 'elwaza'@'localhost';
+
+-- Read-only analyst
+CREATE USER 'analyst'@'localhost' IDENTIFIED BY 'ayora';
+GRANT SELECT ON membership_park.* TO 'analyst'@'localhost';
+
+FLUSH PRIVILEGES;
